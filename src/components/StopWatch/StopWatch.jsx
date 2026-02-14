777ab19b4e82fc9fdb05471ca8414c830a93bb11
Nageshmt <nageshmt72@gmail.com>
2026-02-14 21:55:28 +0530

    Added StopWatch component

---
+++ src/components/StopWatch/StopWatch.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+function StopWatch() {
+
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+
+    useEffect(() => {
+
+        let interval;
+        if (running) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 10)
+            }, 10)
+        } else if (!running) {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval);
+    }, [running])
+
+    return (
+        <div className=" flex flex-col items-center justify-center py-8">
+
+            <h1 className="text-2xl font-semibold pb-2">Stop Watch</h1>
+            <div className="text-xl font-semibold">
+                <span>{("0" + Math.floor((time / 6000) % 60)).slice(-2)}:</span>
+                <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+                <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span>
+            </div>
+            <div className="w-1/3 max-w-sm flex flex-row justify-evenly">
+                {running ? (
+                    <button className="border rounded-lg py-1 px-2" onClick={() => { setRunning(false) }}>Stop</button>
+                ) : (
+
+                    <button className="border rounded-lg py-1 px-2" onClick={() => { setRunning(true) }}>Start</button>
+                )
+                }
+                <button className="border rounded-lg py-1 px-2" onClick={() => { setTime(0) }}>Reset</button>
+            </div>
+        </div>
+    )
+}
+
+export default StopWatch;
